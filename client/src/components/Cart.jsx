31effa8793a5ab756c49d3d9ef470a71f4493895
Druthi <polisettydruthi.k@gmail.com>
2019-04-15 12:36:28 -0700

    Cart added

--- client/src/components/Cart.jsx
+++ client/src/components/Cart.jsx
@@ -3,7 +3,8 @@ import Quantity from './Quantity.jsx';
 
 const Cart = (props) => (
 <div>
-  <button className="btn"><i className="fas fa-shopping-cart"></i> Add To Cart</button>
+<button className="btn" onClick={props.onClick}><i className="fas fa-shopping-cart"></i> Add To Cart</button>
+
 </div>
 );
 
