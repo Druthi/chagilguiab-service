a43cf9014dbefa7b8e87ebf57b397c01694ff52f
Druthi <polisettydruthi.k@gmail.com>
2019-04-15 02:28:00 -0700

    Style changes

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,4 +3,5 @@ import ReactDOM from 'react-dom';
 import App from './components/Home.jsx';
 import $ from 'jquery';
 
+window.ProductDetails = App;
 ReactDOM.render(<App />, document.getElementById('app') || document.createElement('div'));
